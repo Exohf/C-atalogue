cc3d4e411afde04a87473b0d532c5753aecdd92f
Exohf <apedromonteiro@myges.fr>
2023-11-07 03:02:45 +0100

    [MODIFY] remove list function from headers

--- database/database_functions.h
+++ database/database_functions.h
@@ -6,6 +6,5 @@
 sqlite3 *openDatabaseConnection(const char *db_name);
 void closeDatabaseConnection();
 int initializeDatabase(const char *db_name);
-void listTableContents(const char *db_name);
 
 #endif /* DATABASE_DATABASE_FUNCTIONS_H */
