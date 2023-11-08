bb3cee402f443fd1eab7a5ed6b47d26e060804f9
Exohf <apedromonteiro@myges.fr>
2023-11-08 07:30:26 +0100

    [UPDATE] Update build_and_run to new path

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compilation
-gcc -o program main.c database/database_functions.c utils/utils.c -lsqlite3
+gcc -o program main.c database/database_functions.c utils/utils.c validations/validations.c -lsqlite3
 
 # Exécution
 ./program
