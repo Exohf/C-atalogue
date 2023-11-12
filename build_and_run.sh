5f1884d5de71df17a4ca511b1a148d90cfe7dabc
Exohf <apedromonteiro@myges.fr>
2023-11-12 03:09:21 +0100

    [MODIFY] add -d for debug to build and run

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
-# Compilation
-gcc -o program main.c database/database_functions.c utils/utils.c validations/validations.c -lsqlite3
+# Options par défaut
+DEBUG=0
+
+# Analyse des options en ligne de commande
+while getopts ":b" opt; do
+  case $opt in
+    b)
+      DEBUG=1
+      ;;
+    \?)
+      echo "Option invalide: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Compilation avec ou sans options de débogage
+if [ "$DEBUG" -eq 1 ]; then
+  gcc -g -o program main.c database/database_functions.c utils/utils.c validations/validations.c -lsqlite3
+else
+  gcc -o program main.c database/database_functions.c utils/utils.c validations/validations.c -lsqlite3
+fi
 
 # Exécution
-./program
+if [ "$DEBUG" -eq 1 ]; then
+  gdb ./program
+else
+  ./program
+fi
 
 # Suppression de l'exécutable
 rm program
